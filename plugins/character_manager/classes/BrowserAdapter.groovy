ed75acb2c9b397a31591ff97fa9ac5b4fe506fe8
Leonid Kozarin <kozalo@nekochan.ru>
2017-04-28 07:27:15 +0300

    Fixes the freezes on Linux systems when the app is trying to open a URL.

--- plugins/character_manager/classes/BrowserAdapter.groovy
+++ plugins/character_manager/classes/BrowserAdapter.groovy
@@ -1,6 +1,7 @@
 package classes
 
 import java.awt.Desktop
+import java.awt.EventQueue
 
 // Вспомогательный класс для открытия ссылок в браузере.
 // Код основан на следующем ответе: http://stackoverflow.com/a/10967469
@@ -9,11 +10,13 @@ class BrowserAdapter {
     static void openWebpage(URI uri) {
         Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null
         if (desktop?.isSupported(Desktop.Action.BROWSE)) {
-            try {
-                desktop.browse(uri)
-            } catch (Exception e) {
-                e.printStackTrace()
-            }
+            EventQueue.invokeLater({ ->
+                try {
+                    desktop.browse(uri)
+                } catch (Exception e) {
+                    e.printStackTrace()
+                }
+            })
         }
     }
 
